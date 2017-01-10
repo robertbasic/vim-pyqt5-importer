6f543f97b3904ddd66dc6b2b22456e58e4d0cfc2
Robert Basic <robertbasic.com@gmail.com>
2017-01-10 15:52:31 +0100

    Close file after we are done with it

--- lib/pyqt5importer/packages.py
+++ lib/pyqt5importer/packages.py
@@ -4,4 +4,6 @@ def find_class_package(class_name, packages_file_path):
     packages_file = open(packages_file_path, 'r')
     packages = json.load(packages_file)
 
+    packages_file.close()
+
     return packages[class_name] if class_name in packages else False
